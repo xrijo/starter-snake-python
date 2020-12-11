e0edaec2322941980f9c4f1eaddc68d824009238
Jonas Ringsdorf <ringsdorf.jonas@gmail.com>
2020-12-11 13:52:54 +0100

    added username

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class Battlesnake(object):
         # TIP: If you open your Battlesnake URL in browser you should see this data
         return {
             "apiversion": "1",
-            "author": "",  # TODO: Your Battlesnake Username
+            "author": "snacker",  # TODO: Your Battlesnake Username
             "color": "#888888",  # TODO: Personalize
             "head": "default",  # TODO: Personalize
             "tail": "default",  # TODO: Personalize
